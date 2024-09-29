ec05f034290641036fd65f9a64c5f34f0d48f395
ryanvuu <ryanvu077@gmail.com>
2024-09-29 12:33:27 -0700

    Implement teacher functionality

--- main.py
+++ main.py
@@ -1,41 +1,26 @@
-from school_search import searchStudent
+from school_search import *
 
 def printQueryCommands():
     print("\t• S[tudent]: <lastname> [B[us]]\n\t• T[eacher]: <lastname>\n\t• B[us]: <number>\n\t• G[rade]: <number> [H[igh]|L[ow]]\n\t• A[verage]: <number>• I[nfo]\n\t• Q[uit]")
 
 def sourceQuery(user_query: str) -> bool:
     query = user_query.split()
-<<<<<<< HEAD
-
-=======
->>>>>>> 1757aee36f034857ef70b550b20cf89eb9bee577
     query_len = len(query)
     
     if query_len == 0:
         return True
     
-<<<<<<< HEAD
-    # Remove colons
-    student_options = {"S", "Student"}
-    bus_options = {"B", "Bus"}
-
-    if query[0] in student_options:
-        # S[tudent]: <lastname>
-=======
     student_options = {"S:", "Student:", "S"}
     bus_options = {"B", "Bus",}
+    teacher_options = {"T", "Teacher"}
 
     if query[0] in student_options:
->>>>>>> 1757aee36f034857ef70b550b20cf89eb9bee577
         if query_len == 2:
             lastname = query[1].upper()
             searchStudent(lastname)
             return True
 
-<<<<<<< HEAD
         # S[tudent]: <lastname> [B[us]]
-=======
->>>>>>> 1757aee36f034857ef70b550b20cf89eb9bee577
         elif query_len == 3:
             if query[2] in bus_options:
                 lastname = query[1].upper()
@@ -49,6 +34,10 @@ def sourceQuery(user_query: str) -> bool:
             print("That didn't seem to work. Try one of these queries:")
             printQueryCommands()
             return True
+    elif query[0] in teacher_options:
+        lastname = query[1].upper()
+        findTStudents(lastname)
+        return True
     
     elif query[0] == "Q":
         return False
