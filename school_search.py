09cdeaa10ba8a1c2f7a22aad38c9ba0132dcc332
ryanvuu <ryanvu077@gmail.com>
2024-09-30 12:55:59 -0700

    Implement grade search functionality

--- school_search.py
+++ school_search.py
@@ -25,3 +25,29 @@ def findTStudents(lastname: str):
         # '_' means ignore index
         for _, row in df_found.iterrows():
             print(f"{row["StFirstName"]} {row["StLastName"]} is assigned to the class of {row["TFirstName"]} {row["TLastName"]}.")
+
+def findGStudents(number: int, low=False, high=False):
+    df_found = df_students[df_students["Grade"] == number]
+
+    if df_found.empty:
+        return
+    else:
+        if not low and not high:
+            # '_' means ignore index
+            for _, row in df_found.iterrows():
+                print(f"{row["StFirstName"]} {row["StLastName"]} is in grade {row["Grade"]}.")
+        elif low:
+            # Row number of lowest GPA
+            min = df_found["GPA"].idxmin()
+            print(f"{df_found.loc[min, "StFirstName"]} {df_found.loc[min, "StLastName"]}, "
+            f"who takes bus route {df_found.loc[min, "Bus"]}, is assigned to the class of "
+            f"{df_found.loc[min, "TFirstName"]} {df_found.loc[min, "TLastName"]}. {df_found.loc[min, "StFirstName"]} "
+            f"has a GPA of {df_found.loc[min, "GPA"]}.")
+        elif high:
+            # Row number of highest GPA
+            max = df_found["GPA"].idxmax()
+            print(f"{df_found.loc[max, "StFirstName"]} {df_found.loc[max, "StLastName"]}, "
+            f"who takes bus route {df_found.loc[max, "Bus"]}, is assigned to the class of "
+            f"{df_found.loc[max, "TFirstName"]} {df_found.loc[max, "TLastName"]}. {df_found.loc[max, "StFirstName"]} "
+            f"has a GPA of {df_found.loc[max, "GPA"]}.")
+            
